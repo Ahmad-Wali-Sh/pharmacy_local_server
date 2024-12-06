6d2bccf270400a5a9a6d6567fbd312e19fc621ed
Ahmad Wali Sharify <ahmadwalisharifybtc@gmail.com>
2024-12-07 00:23:04 +0430

    prescription after before handle

--- controllers/prescriptionController.js
+++ controllers/prescriptionController.js
@@ -1,7 +1,5 @@
+const Medicine = require('../models/Medicine');
 const Prescription = require('../models/Prescription');
-const Patient = require('../models/Patient');
-const Doctor = require('../models/Doctor');
-const Department = require('../models/Department');
 const PrescriptionThrough = require('../models/PrescriptionThrough');
 
 
@@ -12,6 +10,26 @@ exports.getPrescriptionItems = async (req, res) => {
         res.status(202).json(prescriptionThroughs)
     } catch (err) {
         res.status(500).json({error: err.message})
+    }
+}
+
+
+exports.modifyPrescriptionThroughBody = async (req, res, next) => {
+    if (req.body) {
+        const medicine = await Medicine.findOne({ where: { id: req.body.medician_id }})
+        req.body.each_price = medicine.price
+        req.body.total_price = req.body.each_price * (req.body.quantity || 1)
+    }
+    next();
+};
+
+exports.modifyPrescriptionAfter = async (req, res, next) => {
+    if (req.record) {
+        const prescription = await Prescription.findOne({ where: { id: req.record.id}})
+        prescription.set({
+            prescription_number: `offline-${prescription.id}`
+        })
+        res.status(202).json(prescription)
     }
 }
 
--- routes/prescriptions.js
+++ routes/prescriptions.js
@@ -3,10 +3,10 @@ const authenticate = require('../middlewares/authenticate');
 const GenericController = require('../utils/genericController')
 const Prescription = require('../models/Prescription')
 const PrescriptionThrough = require('../models/PrescriptionThrough')
-const { getPrescriptionItems} = require('../controllers/prescriptionController')
+const { getPrescriptionItems, modifyPrescriptionThroughBody, modifyPrescriptionAfter} = require('../controllers/prescriptionController')
 const router = express.Router();
 
-router.post('/prescription', authenticate, GenericController.handleCreate(Prescription));
+router.post('/prescription', authenticate, GenericController.handleCreate(Prescription, true), modifyPrescriptionAfter);
 router.get('/prescriptions', authenticate, GenericController.handleGetAll(Prescription));
 router.get('/prescription/:id', authenticate, GenericController.handleGetById(Prescription));
 router.patch('/prescription/:id', authenticate, GenericController.handleUpdate(Prescription));
@@ -17,10 +17,10 @@ router.get('/prescriptions/last', authenticate, GenericController.handleLast(Pre
 router.get('/prescription/previous/:id', authenticate, GenericController.handlePrevious(Prescription));
 router.get('/prescription/next/:id', authenticate, GenericController.handleNext(Prescription));
 
-router.post('/prescription-through', authenticate, GenericController.handleCreate(PrescriptionThrough));
+router.post('/prescription-through', authenticate, modifyPrescriptionThroughBody, GenericController.handleCreate(PrescriptionThrough));
 router.get('/prescription-throughs', authenticate, GenericController.handleGetAll(PrescriptionThrough));
 router.get('/prescription-through/:id', authenticate, GenericController.handleGetById(PrescriptionThrough));
-router.patch('/prescription-through/:id', authenticate, GenericController.handleUpdate(PrescriptionThrough));
+router.patch('/prescription-through/:id', authenticate, modifyPrescriptionThroughBody, GenericController.handleUpdate(PrescriptionThrough));
 router.delete('/prescription-through/:id', authenticate, GenericController.handleDelete(PrescriptionThrough));
 router.get('/prescription-throughs/:prescription_id', authenticate, getPrescriptionItems);
 
